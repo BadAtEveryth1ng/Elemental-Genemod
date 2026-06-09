47a8861bdfdb4ed84c803b3bdc364ed7f43b989f
Chinch-Bug <77509218+Chinch-Bug@users.noreply.github.com>
2026-06-09 22:37:13 +0300

    merge issue

--- scripts/cat/cats.py
+++ scripts/cat/cats.py
@@ -3930,6 +3930,13 @@ with open(
 ) as read_file:
     PERMANENT = ujson.loads(read_file.read())
 
+with open(
+    os.path.normpath(f"{_resource_directory}elemental_condition_block.json"),
+    "r",
+    encoding="utf-8",
+) as read_file:
+    ELEMENT_BLOCK = ujson.loads(read_file.read())
+
 
 LEAD_CEREMONY_SC: Optional[Dict] = None
 LEAD_CEREMONY_DF: Optional[Dict] = None
